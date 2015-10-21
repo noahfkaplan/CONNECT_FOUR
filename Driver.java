d29ed733f089926492b5143e3fe6f05b9d0f23e0
noahfkaplan <nkaplan1@uncc.edu>
2015-10-21 15:13:10 -0400

    Driver.java
    the class that has the main method and runs the rest of the application

---
+++ Driver.java
@@ -0,0 +1,33 @@
+/**
+ * Created by Noah on 10/19/2015.
+ */
+public class Driver {
+    public static void main(String args[]) {
+        Board b = new Board();
+        UserInterface GUI = new UserInterface();
+        boolean cont = true;
+        int turnNumber = 1;
+        Player p1 = new Player(1);
+        Player p2 = new Player(2);
+        WinnerCheck check = new WinnerCheck();
+        while (cont == true) {
+            if (turnNumber % 2 == 1) {
+                b = p1.takeTurn(b);
+                GUI.setGrid(b.getGrid());
+                check.checkWinner(b.getGrid(), 1);
+            }
+            else {
+                b = p2.takeTurn(b);
+                GUI.setGrid(b.getGrid());
+                check.checkWinner(b.getGrid(), p2.getColor());
+            }
+            if (b.checkFull() == true || check.getGameWon() == true) {
+                cont = false;
+            }
+            turnNumber++;
+        }
+        if(check.getGameWon() == true){
+            System.out.println("Player " + check.getColorOfWinner() + " wins");
+        }
+    }
+}
