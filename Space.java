58eb7dbe55848abad9ba7f9f874d20fa9c3926c4
noahfkaplan <nkaplan1@uncc.edu>
2015-10-21 15:10:05 -0400

    Space.java
    class definition for a space object

---
+++ Space.java
@@ -0,0 +1,32 @@
+/**
+ * Created by Noah on 10/19/2015.
+ */
+public class Space {
+    private int row;
+    private int column;
+    private int color;
+
+    /**
+     * the overloaded constructor
+     * @param r the row of the space in the grid
+     * @param c the column of the space in the grid
+     * @param co the color of the space (0=empty,1=red,2=black)
+     */
+    public Space(int r, int c, int co){
+        row = r;
+        column = c;
+        color = co;
+    }
+
+    /**
+     * accessor method for the color of the space
+     * @return color, the color of the space
+     */
+    public int getColor(){
+        return color;
+    }
+    public void setColor(int co){
+        color = co;
+    }
+
+}
