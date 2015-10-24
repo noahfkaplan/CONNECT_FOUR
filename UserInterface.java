7561843a135e172000ed2170a34c5d7ddb902df8
noahfkaplan <nkaplan1@uncc.edu>
2015-10-24 13:17:26 -0400

    Update UserInterface.java
    adds the button listener to the buttons on the board so that the user no longer has to type the column into the command line

--- UserInterface.java
+++ UserInterface.java
@@ -9,12 +9,15 @@ public class UserInterface extends JFrame{
     //the only field of my interface class is a 2d array of jbuttons which will act as the pieces on the board
     private JButton[][] buttonGrid;
     private JPanel[][] panelGrid;
+    private Color player1;
+    private Color player2;
     /**
      * constructor method for the interface
      */
-    public UserInterface(){
-        //what will appear at the top of the interface
+    public UserInterface(Color p1,Color p2){
         super("Connect Four");
+        player1 = p1;
+        player2 = p2;
         //setting the layout to a grid 6 tall and 7 wide
         setLayout(new GridLayout(6,7));
         //setting the width and height of the window
@@ -34,6 +37,7 @@ public class UserInterface extends JFrame{
                 JButton button = new JButton("");
                 button.setBackground(Color.WHITE);
                 button.setPreferredSize(new Dimension(60, 60));
+                button.addActionListener(new ButtonListener());
                 //putting the number that needs to be inputted in the bottom button of each column
                 if(r == 0){
                     button.setText(""+ (c+1));
@@ -56,13 +60,27 @@ public class UserInterface extends JFrame{
             for(int c = 0; c <= 6; c++){
                 //if the color of the space object at any location = 1, set the button of that location to red
                 if(grid[r][c].getColor() == 1){
-                    buttonGrid[r][c].setBackground(Color.RED);
+                    buttonGrid[r][c].setBackground(player1);
                 }
                 //if the color of the space object at any location = 2, set the button of that location to black
                 if(grid[r][c].getColor() == 2){
-                    buttonGrid[r][c].setBackground(Color.BLACK);
+                    buttonGrid[r][c].setBackground(player2);
                 }
             }
+        }
+    }
+    private class ButtonListener implements ActionListener {
+        public void actionPerformed (ActionEvent e){
+            //Execute when button is pressed
+            for(int r = 0; r < 6; r++) {
+                for (int c = 0; c < 7; c++) {
+                    if(e.getSource() == buttonGrid[r][c] && TurnControl.b.getColor(r,c)==0 && TurnControl.gameOver == false){
+                        TurnControl.step(c);
+                    }
+                }
+            }
+
+
         }
     }
 }
